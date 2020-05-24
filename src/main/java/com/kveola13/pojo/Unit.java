ab814e96b367e48dca356951ee6c6e2bfaa99cd8
olakv <kveola13@student.westerdals.no>
2020-05-24 17:06:37 +0200

    added a unit

---
+++ src/main/java/com/kveola13/pojo/Unit.java
@@ -0,0 +1,50 @@
+package com.kveola13.pojo;
+
+public class Unit {
+    int uId;
+    String name;
+    int attack;
+    int health;
+
+    public Unit() {
+    }
+
+    public Unit(int uId, String name, int attack, int health) {
+        this.uId = uId;
+        this.name = name;
+        this.attack = attack;
+        this.health = health;
+    }
+
+    public int getuId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
