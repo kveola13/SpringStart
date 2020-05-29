b0db56b358ca813818bcccbad38278ff441052b2
olakv <kveola13@student.westerdals.no>
2020-05-29 22:31:27 +0200

    finished educational tutorial

--- src/main/java/com/kveola13/Application.java
+++ src/main/java/com/kveola13/Application.java
@@ -1,5 +1,6 @@
 package com.kveola13;
 
+import com.kveola13.pojo.Race;
 import com.kveola13.pojo.Unit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +12,7 @@ public class Application {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         Unit unit = context.getBean(Unit.class);
         System.out.println(unit.init());
+        Unit unit1 = new Unit(101, "Murloc one", 3, 1, new Race("Murloc", 2));
+        System.out.println(unit1.toString());
     }
 }
